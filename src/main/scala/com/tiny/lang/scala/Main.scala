409daaf27e7395c1ce09267f9e7013775c613f92
chenglong.wang <chenglong.wang@tendcloud.com>
2019-02-19 20:29:32 +0800

    scala package object, syntax testing
    Change-Id: I42a4b0616c1057b6b1e948fc11a217261f5284e7

--- src/main/scala/com/tiny/lang/scala/Main.scala
+++ src/main/scala/com/tiny/lang/scala/Main.scala
@@ -1,19 +1,9 @@
 package com.tiny.lang.scala
 
-import com.sun.tools.javac.util.ListBuffer
-import com.tiny.lang.scala.generic.Food
-
 object Main {
 
   def main(args: Array[String]): Unit = {
-    val name3 = new ListBuffer
-    /*val name2 = new ListBuffer[_ <: Any]
-    val name1 = new ListBuffer[_ <: Fruit]
-    val buf4 = new ListBuffer[_ <: Fruit with Medicine with Serializable]*/
+    implicitly[List[String]].foreach(println)
   }
-}
 
-/**
-  *
-  */
-class A[_ <: Food]
+}
---
+++ src/main/scala/com/tiny/lang/scala/package.scala
@@ -0,0 +1,6 @@
+package com.tiny.lang
+
+package object scala {
+
+  implicit val ab: List[String] = List("A", "B")
+}
